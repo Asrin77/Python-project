3c812a4494391ed66f311e5d6da0b77cda80476a
sadianishat <sadianishataiub@gmail.com>
2026-07-16 21:32:06 +0600

    update-2

--- background.py
+++ background.py
@@ -2,7 +2,7 @@ import pygame as game
 
 game.init()
 
-window = game.display.set_mode((1500, 600))
+window = game.display.set_mode((1000, 400))
 
 speed = game.time.Clock()
 
@@ -13,6 +13,8 @@ while running:
         if event.type == game.QUIT:
             running = False
 
-    speed.tick(60)
     window.fill("white")
+    game.display.flip()
+
+    speed.tick(60)   
 game.quit()
