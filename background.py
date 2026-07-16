6d9e3e364458516c1a34437f8b8c283d4bd5669e
sadianishat <sadianishataiub@gmail.com>
2026-07-16 22:43:14 +0600

    added image

--- background.py
+++ background.py
@@ -3,17 +3,24 @@ import pygame as game
 game.init()
 
 window = game.display.set_mode((1000, 400))
+game.display.set_caption("Piko runs")
+background_image = game.image.load("skybackground.png").convert()
 
 speed = game.time.Clock()
+
+
 
 running = True
 
 while running:
+
+    window.blit(background_image, (0,0))
+
     for event in game.event.get():
         if event.type == game.QUIT:
             running = False
 
-    window.fill("white")
+    
     game.display.flip()
 
     speed.tick(60)   
