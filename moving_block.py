740df0f1e084c92cac95b35f19bc38b79de98ae9
sadianishat <sadianishataiub@gmail.com>
2026-07-17 16:32:36 +0600

    added background image

--- moving_block.py
+++ moving_block.py
@@ -1,4 +1,22 @@
 import pygame as game
 
+game.init()
 
+speed = game.time.Clock()
 
+window = game.display.set_mode((600, 600))
+background_image = game.image.load("grass.jpg")
+
+running = True
+
+while running:
+    for event in game.event.get():
+        if event.type == game.QUIT:
+            running = False
+
+    window.blit(background_image, (0,0))
+
+    game.display.flip()
+
+    speed.tick(60)
+game.quit()
