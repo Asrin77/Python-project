af8324de4dce11fd505b8a5e661d8f66a01a6cd3
sadianishat <sadianishataiub@gmail.com>
2026-07-17 12:55:53 +0600

    scrolling background

--- background.py
+++ background.py
@@ -1,25 +1,34 @@
 import pygame as game
 
 game.init()
-
-window = game.display.set_mode((1000, 400))
-game.display.set_caption("Piko runs")
-background_image = game.image.load("skybackground.png").convert()
-
 speed = game.time.Clock()
 
+window = game.display.set_mode((1000, 400))
 
+background_image = game.image.load("skybackground.png")
+b_width = background_image.get_width()
+image_pos = (window.get_width()/b_width) + 2
+
+scroll = 0
 
 running = True
 
 while running:
 
     window.blit(background_image, (0,0))
-
-    for event in game.event.get():
+    
+    for event in game.event.get():  #close button
         if event.type == game.QUIT:
             running = False
+    
+    
+    for i in range(0,int(image_pos)):  #scrolling background
+        window.blit(background_image, ((i*b_width+scroll , 0)))
+    
+    scroll -= 4
 
+    if scroll <= -b_width:  #resetting
+        scroll = 0
     
     game.display.flip()
 
