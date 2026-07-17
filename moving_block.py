05a39e995ddd7bb83ac74e91a4a7428c7118da9b
sadianishat <sadianishataiub@gmail.com>
2026-07-17 17:45:44 +0600

    fixed issues

--- moving_block.py
+++ moving_block.py
@@ -22,19 +22,15 @@ except FileNotFoundError:
     exit()
 
 
-X, Y = 100,100
+X = 100
+Y = 100
 def block_design():
-    window.fill((110, 110, 5)) 
+    window.blit(background_image, (0,0)) 
     window.blit(block, (X, Y))
-    game.display.update()
 
 running = True
 
 while running:
-
-    window.blit(background_image, (0,0))
-
-    
 
     for event in game.event.get():
         if event.type == game.QUIT:
@@ -42,16 +38,17 @@ while running:
         if event.type == game.KEYDOWN:
             if event.key == game.K_RIGHT:
                 X+=6
-                block_design()
+    
             if event.key == game.K_LEFT:
                 X -=6
-                block_design()
+                
             if event.key == game.K_UP:
                 Y -= 6
-                block_design()
+                
             if event.key == game.K_DOWN:
                 Y += 6
-                block_design()
+                
+    block_design()
 
     game.display.flip()
     speed.tick(60)
