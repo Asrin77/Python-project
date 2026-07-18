d8c7446445fa22419b89011f19c0a4d7fbc22f0a
sadianishat <sadianishataiub@gmail.com>
2026-07-18 19:33:40 +0600

    adding snake design

--- moving_block.py
+++ moving_block.py
@@ -2,10 +2,18 @@ import pygame as game
 
 game.init()
 
-class Window_design:
+class Snake_design:
+    def __init__(this):
+        this.block = game.load.("game-images/yellow_block.png")
+        this.x = 100
+        this.y = 100
+        
+
+class Game_design:
     def __init__ (this, window):
         this.screen = window
         this.window = game.display.set_mode((600, 600))
+
 
 
 
