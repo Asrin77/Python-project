3357ecf75a2c00645046613c446d00a763357944
sadianishat <sadianishataiub@gmail.com>
2026-07-18 19:44:51 +0600

    adding moving keys for snake

--- moving_block.py
+++ moving_block.py
@@ -4,15 +4,28 @@ game.init()
 
 class Snake_design:
     def __init__(this):
-        this.block = game.load.("game-images/yellow_block.png")
+        this.block = game.load("game-images/yellow_block.png")
         this.x = 100
         this.y = 100
+
+    def right_key(this):
+        this.x += 12
+
+    def left_key(this):
+        this.x -= 12
+
+    def up_key(this):
+        this.y -= 12
+
+    def down_key(this):
+        this.y += 12
         
 
 class Game_design:
     def __init__ (this, window):
         this.screen = window
         this.window = game.display.set_mode((600, 600))
+        this.window = game.image.load("game-images/grass.jpg")
 
 
 
