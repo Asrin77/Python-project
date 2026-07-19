1a41b43e6b8505d0702babb1e9e1d7b82f575291
Sayela Islam <mistus481@gmail.com>
2026-07-19 19:40:34 +0600

    Apple class

--- build_snake_draw_apple.py
+++ build_snake_draw_apple.py
@@ -4,3 +4,17 @@ import time
 import random
 
 SIZE = 40
+class Apple:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.image = pygame.image.load("resources/apple.jpg").convert()
+        self.x = 120
+        self.y = 120
+
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
+
+    def move(self):
+        self.x = random.randint(1,25)*SIZE
+        self.y = random.randint(1,20)*SIZE
