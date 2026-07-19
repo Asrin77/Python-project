44eda3d8b0c5894b5712659a4edefda909c508b2
sadianishat <sadianishataiub@gmail.com>
2026-07-19 17:56:01 +0600

    changed

--- moving_snake.py
+++ moving_snake.py
@@ -8,40 +8,40 @@ class Snake_design:
         this.screen = screen
 
         try:
-            this.rb = game.image.load("game-images/rabbit.png")
+            this.sk = game.image.load("game-images/yellow_snake.png")
         except FileNotFoundError:
-            print(f"Error: rabbit.png not found")
+            print(f"Error: yellow_snake.png not found")
             game.quit()
             exit()
  
-        this.X = 236 # 600 -128 / 2
-        this.Y = 236
+        this.X = 100 
+        this.Y = 100
 
     def right_key(this):
         if this.X < 600:
-            this.X += 2
+            this.X += 15
 
     def left_key(this):
         if this.X > 0:
-            this.X -= 2
+            this.X -= 15
 
     def up_key(this):
         if this.Y > 0:
-            this.Y -= 2
+            this.Y -= 15
 
     def down_key(this):
         if this.Y < 600 :  
-            this.Y += 2
+            this.Y += 15
     
     def display(this):
-        this.screen.blit(this.rb, (this.X, this.Y))
+        this.screen.blit(this.sk, (this.X, this.Y))
 
         
 
 class Game_design:
     def __init__ (this):
         this.screen = game.display.set_mode((600, 600))
-        this.rabbit= Rabbit_design(this.screen)
+        this.snake= Snake_design(this.screen)
          
         try:
             this.window = game.image.load("game-images/grass.jpg")
@@ -53,46 +53,33 @@ class Game_design:
 
     def run(this):
         running = True
-
-        key_actions = {
-            game.K_RIGHT: this.rabbit.right_key,
-            game.K_LEFT: this.rabbit.left_key,
-            game.K_UP: this.rabbit.up_key,
-            game.K_DOWN: this.rabbit.down_key
-        }
         
         while running:
             for event in game.event.get():
                 if event.type == game.QUIT:
                      running = False
 
-                # if event.type == game.KEYDOWN:
+                if event.type == game.KEYDOWN:
 
-                #     if event.key == game.K_RIGHT:
-                #         this.rabbit.right_key()
+                    if event.key == game.K_RIGHT:
+                        this.snake.right_key()
                     
-                #     if event.key == game.K_LEFT:
-                #         this.rabbit.left_key()
+                    if event.key == game.K_LEFT:
+                        this.snake.left_key()
                     
-                #     if event.key == game.K_UP:
-                #         this.rabbit.up_key()
+                    if event.key == game.K_UP:
+                        this.snake.up_key()
 
-                #     if event.key == game.K_DOWN:
-                #         this.rabbit.down_key()
-            keys = game.key.get_pressed()
-            
-            for k in key_actions:
-                if keys[k]:
-                    key_actions[k]()
-
-                
+                    if event.key == game.K_DOWN:
+                        this.snake.down_key()
+  
             this.screen.blit(this.window, (0,0))
-            this.rabbit.display()
+            this.snake.display()
             speed.tick(60)
             game.display.flip()
         game.quit()
         
 
-rb = Game_design()
-rb.run()
+s = Game_design()
+s.run()
                         
