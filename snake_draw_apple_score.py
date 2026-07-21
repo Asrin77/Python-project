8dd7094a766b9c5392dce9850b37b8d9ee780db1
sadianishat <sadianishataiub@gmail.com>
2026-07-21 21:41:39 +0600

    fixing issue

--- snake_draw_apple_score.py
+++ snake_draw_apple_score.py
@@ -87,13 +87,13 @@ class Game:
         self.font = pygame.font.SysFont("Arial", 30)
 
     # Added Score Display Method
-    def display_score(self):
-        score_text = self.font.render(
-            "Score : " + str(self.score),
-            True,
-            (255, 255, 255))
-        self.surface.blit(score_text, (20, 20))
-        pygame.display.flip()
+    # def display_score(self):
+    #     score_text = self.font.render(
+    #         "Score : " + str(self.score),
+    #         True,
+    #         (255, 255, 255))
+    #     self.surface.blit(score_text, (20, 20))
+    #     pygame.display.flip()
 
     
     def play(self):
