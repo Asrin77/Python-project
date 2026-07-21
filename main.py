b296fb9e154382cd46515ebc17516628470cb3cd
sadianishat <sadianishataiub@gmail.com>
2026-07-21 22:42:12 +0600

    adding variable value and exception hadling

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import pygame as game
+import pygame
+import random
+
+size = 40
+height = 40
+width = 40
+
+HS_FILE = "highscore.txt"
+
+class Apple:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+
+        try:
+            self.apple_image = pygame.image.load("game-images/apple.png").convert_alpha()
+        except FileNotFoundError:
+            print("Error: apple.png not found")
+            pygame.quit()
+            exit()
